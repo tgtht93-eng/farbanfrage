2e5c18aae724d963ed7a6dc2fa76dda40e965ee3
tgtht93-eng <tgtht93@gmail.com>
2026-04-26 22:35:27 +0200

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import FarbanfrageTheken from "./FarbanfrageTheken.jsx";
+
+createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <FarbanfrageTheken />
+  </React.StrictMode>
+);
